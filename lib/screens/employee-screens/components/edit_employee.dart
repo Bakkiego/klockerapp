e26d047a39ac83f28837a74446dbfc6b7ab14358
Daniel <lekeayomidotun@gmail.com>
2025-10-15 12:04:39 +0200

    Editing employee's details view

--- lib/screens/employee-screens/components/edit_employee.dart
+++ lib/screens/employee-screens/components/edit_employee.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'employee_tile.dart';
 
-class EditEmployee extends StatelessWidget {
+class EditEmployee extends StatefulWidget {
   const EditEmployee({super.key});
 
   @override
+  State<EditEmployee> createState() => _EditEmployeeState();
+}
+
+class _EditEmployeeState extends State<EditEmployee> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder(child: Text('EditEmployee'));
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SearchBar(
+                  hintText: "Employee name",
+                  trailing: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.search),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        EmployeeTile(() {}),
+      ],
+    );
   }
 }
