6d1fd11a6f2b32f388dc6246606528a15e1ca3a2
Daniel <lekeayomidotun@gmail.com>
2025-10-21 19:36:16 +0200

    Time management hub for companies

--- lib/screens/employee-screens/time_management.dart
+++ lib/screens/employee-screens/time_management.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:klockerapp/screens/employee-screens/components/time_management_components/rooster_screen.dart';
+import 'package:klockerapp/screens/employee-screens/components/time_management_components/attendance_screen.dart';
+import 'package:klockerapp/screens/employee-screens/components/time_management_components/leave_screen.dart';
 
-class TimeManagement extends StatelessWidget {
+class TimeManagement extends StatefulWidget {
   const TimeManagement({super.key});
 
   @override
+  State<TimeManagement> createState() => _TimeManagementState();
+}
+
+class _TimeManagementState extends State<TimeManagement> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder(child: Text('Time Management'));
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Time Management'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(child: Text("Attendance")),
+              Tab(child: Text("Rooster")),
+              Tab(child: Text("Leave")),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [AttendanceScreen(), RoosterScreen(), LeaveScreen()],
+        ),
+      ),
+    );
   }
 }
