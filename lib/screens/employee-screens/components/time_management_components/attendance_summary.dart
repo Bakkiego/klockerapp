90502cac84f9be54ffb4c35fff656f3c29195d86
Daniel <lekeayomidotun@gmail.com>
2025-10-28 17:05:48 +0200

    Cleaned up some code for reusability

--- lib/screens/employee-screens/components/time_management_components/attendance_summary.dart
+++ lib/screens/employee-screens/components/time_management_components/attendance_summary.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:klockerapp/utils/custom_theme/text_theme.dart';
+import 'utils/report_summary.dart';
 
 class AttendanceSummary extends StatelessWidget {
   const AttendanceSummary({super.key});
@@ -31,61 +32,6 @@ class AttendanceSummary extends StatelessWidget {
           ),
         ),
       ],
-    );
-  }
-}
-
-class SummaryContainer extends StatelessWidget {
-  SummaryContainer(
-    @required this.sumIcon,
-    @required this.sumValue,
-    @required this.sumTitle,
-  );
-
-  late String sumTitle;
-  late int sumValue;
-  late IconData sumIcon;
-
-  late String sumValueString = sumValue.toString();
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.lightGreenAccent,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      width: 105,
-      height: 120,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 8.0),
-            child: CircleAvatar(
-              radius: 15,
-              backgroundColor: Colors.green,
-              child: Icon(sumIcon),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0, left: 10.0),
-            child: Text(
-              sumTitle,
-              style: TextStyle(color: Colors.black, fontFamily: 'Poppins'),
-              textAlign: TextAlign.left,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10.0),
-            child: Text(
-              sumValueString,
-              style: TextStyle(color: Colors.black, fontFamily: 'Poppins'),
-              textAlign: TextAlign.left,
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
