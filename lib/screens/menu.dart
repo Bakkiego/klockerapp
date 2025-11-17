759c90a73296f15567cfa4fee84b2cf453211471
danielhomiee1 <Daniel@Homiee300.onmicrosoft.com>
2025-11-17 16:59:51 +0200

    Added Dropdown submenu for HRM system navigation

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:klockerapp/components/menu_item_design.dart';
+import 'package:klockerapp/components/menu_expansion_tile.dart';
 import 'package:klockerapp/screens/employee-screens/time_management.dart';
+import 'package:klockerapp/screens/hr-screens/announcements_screen.dart';
+import 'package:klockerapp/screens/hr-screens/awards_screen.dart';
+import 'package:klockerapp/screens/hr-screens/promotion_screen.dart';
+import 'package:klockerapp/screens/hr-screens/resignation_screen.dart';
+import 'package:klockerapp/screens/hr-screens/termination_screen.dart';
+import 'package:klockerapp/screens/hr-screens/transfer_screen.dart';
+import 'package:klockerapp/screens/hr-screens/warning_screen.dart';
 import 'package:klockerapp/utils/custom_theme/text_theme.dart';
 import 'package:klockerapp/screens/employee-screens/employees.dart';
 
@@ -9,6 +17,73 @@ class Menu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<MenuExpansionOBJ> hrmList = [
+      MenuExpansionOBJ(
+        title: 'Awards',
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AwardsScreen()),
+          );
+        },
+      ),
+      MenuExpansionOBJ(
+        title: 'Termination',
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const TerminationScreen()),
+          );
+        },
+      ),
+      MenuExpansionOBJ(
+        title: 'Transfer',
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const TransferScreen()),
+          );
+        },
+      ),
+      MenuExpansionOBJ(
+        title: 'Warning',
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const WarningScreen()),
+          );
+        },
+      ),
+      MenuExpansionOBJ(
+        title: 'Announcements',
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const AnnouncementsScreen(),
+            ),
+          );
+        },
+      ),
+      MenuExpansionOBJ(
+        title: 'Promotion',
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const PromotionScreen()),
+          );
+        },
+      ),
+      MenuExpansionOBJ(
+        title: 'Resignation',
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const ResignationScreen()),
+          );
+        },
+      ),
+    ];
     return ListView(
       children: [
         Title(
@@ -34,12 +109,13 @@ class Menu extends StatelessWidget {
             MaterialPageRoute(builder: (context) => const TimeManagement()),
           );
         }),
-        MenuItemDesign('HRM', Icons.perm_contact_calendar, () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const Employees()),
-          );
-        }),
+        // MenuItemDesign('HRM', Icons.perm_contact_calendar, () {
+        //   Navigator.push(
+        //     context,
+        //     MaterialPageRoute(builder: (context) => const Employees()),
+        //   );
+        // })
+        MenuExpansionTile(hrmList, "HRM", Icons.perm_contact_calendar),
         SizedBox(height: 30),
         Padding(
           padding: const EdgeInsets.all(16.0),
