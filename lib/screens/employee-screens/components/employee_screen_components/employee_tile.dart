a98a9b10b9f2d72d265051af4a677103af22b5c3
danielhomiee1 <Daniel@Homiee300.onmicrosoft.com>
2025-11-17 16:59:19 +0200

    Added better UI feel and look

--- lib/screens/employee-screens/components/employee_screen_components/employee_tile.dart
+++ lib/screens/employee-screens/components/employee_screen_components/employee_tile.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 class EmployeeTile extends StatefulWidget {
-  late VoidCallback onTap;
-  EmployeeTile(this.onTap);
+  VoidCallback onTap;
+  String employeeName;
+  Widget actionButton;
+  EmployeeTile(this.onTap, this.employeeName, this.actionButton, {super.key});
 
   @override
   State<EmployeeTile> createState() => _EmployeeTileState();
@@ -13,17 +15,9 @@ class _EmployeeTileState extends State<EmployeeTile> {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      onTap: () => (),
-      title: Text("John"),
-      trailing: Row(
-        mainAxisSize: MainAxisSize
-            .min, // Prevents the Row from taking all available space
-        children: const [
-          Icon(Icons.edit_outlined), // First icon (e.g., Edit)
-          SizedBox(width: 8), // Add some space between icons
-          Icon(Icons.delete_outline, color: Colors.red),
-        ],
-      ),
+      onTap: widget.onTap,
+      title: Text(widget.employeeName),
+      trailing: widget.actionButton,
     );
   }
 }
