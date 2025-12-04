1f1ee2ee61a60216197f0e2ab494b467ba8f6f79
danielhomiee1 <Daniel@Homiee300.onmicrosoft.com>
2025-12-04 11:38:35 +0200

    Added a few menu dropdowns for some of the screens navigation

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -10,7 +10,21 @@ import 'package:klockerapp/screens/hr-screens/termination_screen.dart';
 import 'package:klockerapp/screens/hr-screens/transfer_screen.dart';
 import 'package:klockerapp/screens/hr-screens/warning_screen.dart';
 import 'package:klockerapp/utils/custom_theme/text_theme.dart';
+import 'company-screens/branch_screen.dart';
+import 'company-screens/department_screen.dart';
+import 'company-screens/payslip_screen.dart';
+import 'company-screens/salary_screen.dart';
 import 'package:klockerapp/screens/employee-screens/employees.dart';
+import 'package:klockerapp/screens/finance-screen/accounts_list_screen.dart';
+import 'package:klockerapp/screens/finance-screen/deposit_screen.dart';
+import 'package:klockerapp/screens/finance-screen/expense_screen.dart';
+import 'package:klockerapp/screens/finance-screen/payee_screen.dart';
+import 'package:klockerapp/screens/finance-screen/payers_screen.dart';
+import 'package:klockerapp/screens/finance-screen/transfer_balance_screen.dart';
+import 'package:klockerapp/screens/finance-screen/company_screen.dart';
+import 'package:klockerapp/screens/finance-screen/report_screen.dart';
+import 'package:klockerapp/screens/help-screens/settings_screen.dart';
+import 'package:klockerapp/screens/help-screens/help_screen.dart';
 
 class Menu extends StatelessWidget {
   const Menu({super.key});
@@ -84,6 +98,93 @@ class Menu extends StatelessWidget {
         },
       ),
     ];
+    List<MenuExpansionOBJ> financeList = [
+      MenuExpansionOBJ(
+        title: 'Accounts List',
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AccountsListScreen()),
+          );
+        },
+      ),
+      MenuExpansionOBJ(
+        title: 'Payee',
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const PayeeScreen()),
+          );
+        },
+      ),
+      MenuExpansionOBJ(
+        title: 'Payers',
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const PayersScreen()),
+          );
+        },
+      ),
+      MenuExpansionOBJ(
+        title: 'Deposit',
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const DepositScreen()),
+          );
+        },
+      ),
+      MenuExpansionOBJ(
+        title: 'Expense',
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const ExpenseScreen()),
+          );
+        },
+      ),
+      MenuExpansionOBJ(
+        title: 'Transfer Balance',
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const ManageTransferScreen(),
+            ),
+          );
+        },
+      ),
+    ];
+    List<MenuExpansionOBJ> companyList = [
+      MenuExpansionOBJ(
+        title: 'Branch & Departments',
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const CompanyScreen()),
+          );
+        },
+      ),
+      MenuExpansionOBJ(
+        title: "Salary",
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const SalaryScreen()),
+          );
+        },
+      ),
+      MenuExpansionOBJ(
+        title: "Payslip",
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const PayslipScreen()),
+          );
+        },
+      ),
+    ];
     return ListView(
       children: [
         Title(
@@ -124,9 +225,18 @@ class Menu extends StatelessWidget {
             style: KAppTextTheme.darkTextTheme.headlineMedium,
           ),
         ),
-        MenuItemDesign('Finance', Icons.monetization_on, () => {}),
-        MenuItemDesign('Company', Icons.business_rounded, () => {}),
-        MenuItemDesign('Report', Icons.auto_graph_rounded, () => {}),
+        MenuExpansionTile(financeList, "Finance", Icons.monetization_on),
+        MenuExpansionTile(companyList, "Company", Icons.business),
+        MenuItemDesign(
+          'Report',
+          Icons.auto_graph_rounded,
+          () => {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const ReportScreen()),
+            ),
+          },
+        ),
         SizedBox(height: 30),
         Padding(
           padding: const EdgeInsets.all(16.0),
@@ -135,8 +245,26 @@ class Menu extends StatelessWidget {
             style: KAppTextTheme.darkTextTheme.headlineMedium,
           ),
         ),
-        MenuItemDesign('Support', Icons.add_ic_call_outlined, () => {}),
-        MenuItemDesign('Help', Icons.add_comment, () => {}),
+        MenuItemDesign(
+          'Support',
+          Icons.add_ic_call_outlined,
+          () => {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SettingsScreen()),
+            ),
+          },
+        ),
+        MenuItemDesign(
+          'Help',
+          Icons.add_comment,
+          () => {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const HelpScreen()),
+            ),
+          },
+        ),
       ],
     );
   }
