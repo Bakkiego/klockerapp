61a826588a6aef3d5f0f886f03cce6c36e70dd73
danielhomiee1 <Daniel@Homiee300.onmicrosoft.com>
2025-12-11 18:41:47 +0200

    made the appbar when it's on light mode look better

--- lib/utils/custom_theme/appbar_theme.dart
+++ lib/utils/custom_theme/appbar_theme.dart
@@ -8,11 +8,19 @@ class KAppBarTheme {
     elevation: 0,
     centerTitle: false,
     scrolledUnderElevation: 0,
-    backgroundColor: Colors.transparent,
+    backgroundColor: Colors.lightGreenAccent,
     surfaceTintColor: Colors.transparent,
     iconTheme: IconThemeData(color: Colors.lightGreenAccent, size: 25),
     titleTextStyle: KAppTextTheme.lightTextTheme.titleLarge,
+    // --- ADDED: Rounded Corners ---
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.only(
+        bottomLeft: Radius.circular(16.0),
+        bottomRight: Radius.circular(16.0),
+      ),
+    ),
   );
+
   static AppBarTheme darkAppBarTheme = AppBarTheme(
     elevation: 0,
     centerTitle: false,
@@ -21,5 +29,12 @@ class KAppBarTheme {
     surfaceTintColor: Colors.black,
     iconTheme: IconThemeData(color: Colors.lightGreenAccent, size: 25),
     titleTextStyle: KAppTextTheme.darkTextTheme.titleLarge,
+    // --- ADDED: Rounded Corners ---
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.only(
+        bottomLeft: Radius.circular(16.0),
+        bottomRight: Radius.circular(16.0),
+      ),
+    ),
   );
 }
